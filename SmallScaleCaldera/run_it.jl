5595ecefa94f53b5db89aa0f2996bd9f75c9f852
Pascal Aellig <pascal.aellig@hotmail.com>
2024-12-16 15:54:57 +0100

    update run_it to correct path

--- SmallScaleCaldera/run_it.jl
+++ SmallScaleCaldera/run_it.jl
@@ -2,7 +2,7 @@ function main()
 
     conduit, depth, radius, ar, extension = 0.2e0, 5e0, 1.25e0, 2, 1e-15
 
-    run(`$(Base.julia_cmd()) --project=. -O3 --startup-file=no --check-bounds=no miniapps/benchmarks/stokes2D/Volcano2D/Caldera2D.jl $(conduit) $(depth) $(radius) $(ar) $(extension)`)
+    run(`$(Base.julia_cmd()) --project=. -O3 --startup-file=no --check-bounds=no Caldera2D.jl $(conduit) $(depth) $(radius) $(ar) $(extension)`)
 
 end
 
