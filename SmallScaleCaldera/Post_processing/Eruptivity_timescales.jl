5f594545f29eb670230ac73f5d1c87adf3fd1f89
Pascal Aellig <pascal.aellig@hotmail.com>
2026-03-05 12:18:51 +0100

    update again -> move to ellipsoid volume

--- SmallScaleCaldera/Post_processing/Eruptivity_timescales.jl
+++ SmallScaleCaldera/Post_processing/Eruptivity_timescales.jl
@@ -1,8 +1,6 @@
-# Townsend et al 2019 calculations of relaxation and eruption criteria
+using JustRelax, JustRelax.JustRelax2D, JustRelax.DataIO
 
-using JLD2, JustRelax, JustRelax.JustRelax2D, JustRelax.DataIO
-
-using CairoMakie, CSV, XLSX, DataFrames, LsqFit
+using CairoMakie, XLSX, DataFrames
 
 data = DataFrame(XLSX.readtable("Onset_of_caldera_collapse_CSV.xlsx", "Systematics"))
 # data = DataFrame(XLSX.readtable("Onset_of_caldera_collapse_CSV.xlsx", "Reference_run_variations"))
@@ -30,7 +28,7 @@ total_radius = radius_chamber .* aspect_ratio
 density_host_rock = 2700.0 # kg/m^3
 gravity = 9.81 # m/s^2
 ΔPc = 20e6 # cirtical overpressure in Pa after Townsend et al 2019
-thermal_diffusivity = 3.0/(2700.0 * 1050.0) # [W/mK] / [kg/m^3] * [J/kgK] = m^2/s
+thermal_diffusivity = 3.0/(density_host_rock * 1050.0) # [W/mK] / [kg/m^3] * [J/kgK] = m^2/s
 η_host_rock_Townsend2019 = 1e19 # Pa s
 η_host_rock_JustCollapse_strong = 1e22 # Pa s
 η_host_rock_JustCollapse_weak = 1e20 # Pa s
@@ -38,26 +36,27 @@ density_magma = 2400.0 # kg/m^3
 M_in_normal = 3e-3 # [km3/yr]
 M_in_increased = 1e-2 # [km3/yr]
 # JustCollpase.jl ellipsoid chamber setup V = 4/3pi * a*b*a
-V0 = @. 4/3 * π * (radius_chamber*1e3 * aspect_ratio)^2 * radius_chamber*1e3   # volume of the chamber in m^3
+V0 = @. (radius_chamber*1e3 * aspect_ratio)^2 * radius_chamber*1e3   # volume of the chamber in m^3
 V0_km3 = V0 ./ 1e9
-
+radius_ref_chamber = @. (1750 * 2.5)^2 * 1750 # volume of the reference chamber in km^3
+radius_chamber = V0
 
 M_injection_normal_T2019 = (M_in_normal * 1e9 / 3.1536e7) * density_magma # kg/s
 M_injection_increased_T2019 = (M_in_increased * 1e9 / 3.1536e7) * density_magma # kg/s
 reference_model_V = @. 4/3 * π * (1750 * 2.5) * 1750 * (1750 * 2.5)
 
 θ1_ref_normal = (3/4π) * (M_injection_normal_T2019 *inv(thermal_diffusivity*density_magma*(1750*2.5)))
-θ2_ref_normal_strong = (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_strong) *inv(ΔPc * density_magma * (1750 * 2.5)^3)
-θ2_ref_normal_weak = (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_weak) *inv(ΔPc * density_magma * (1750 * 2.5)^3)
+θ2_ref_normal_strong = (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_strong) *inv(ΔPc * density_magma * radius_ref_chamber)
+θ2_ref_normal_weak = (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_weak) *inv(ΔPc * density_magma * radius_ref_chamber)
 
 θ1_ref_increased = (3/4π) * (M_injection_increased_T2019 * inv(thermal_diffusivity * density_magma * (1750 * 2.5)))
-θ2_ref_increased_strong = (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_strong) *inv(ΔPc * density_magma * (1750 * 2.5)^3)
-θ2_ref_increased_weak = (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_weak) *inv(ΔPc * density_magma * (1750 * 2.5)^3)
+θ2_ref_increased_strong = (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_strong) *inv(ΔPc * density_magma * radius_ref_chamber)
+θ2_ref_increased_weak = (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_weak) *inv(ΔPc * density_magma * radius_ref_chamber)
 
 θ1_ref_townsend = (3/4π) * (M_injection_normal_T2019 *inv(thermal_diffusivity*density_magma*(1750*2.5)))
-θ2_ref_townsend = (3/4π) * (M_injection_normal_T2019 * η_host_rock_Townsend2019) *inv(ΔPc * density_magma * (1750 * 2.5)^3)
+θ2_ref_townsend = (3/4π) * (M_injection_normal_T2019 * η_host_rock_Townsend2019) *inv(ΔPc * density_magma * radius_ref_chamber)
 θ1_ref_increased_townsend = (3/4π) * (M_injection_increased_T2019 * inv(thermal_diffusivity * density_magma * (1750 * 2.5)))
-θ2_ref_increased_townsend = (3/4π) * (M_injection_increased_T2019 * η_host_rock_Townsend2019) *inv(ΔPc * density_magma * (1750 * 2.5)^3)
+θ2_ref_increased_townsend = (3/4π) * (M_injection_increased_T2019 * η_host_rock_Townsend2019) *inv(ΔPc * density_magma * radius_ref_chamber)
 
 τ_in_T2019_normal = (density_magma * V0) *inv(M_injection_normal_T2019)  # [kg/m3] * [m3] / [kg/s] = [s]
 τ_in_T2019_increased = (density_magma * V0) *inv(M_injection_increased_T2019)  # [kg/m3] * [m3] / [kg/s] = [s]
@@ -69,14 +68,14 @@ reference_model_V = @. 4/3 * π * (1750 * 2.5) * 1750 * (1750 * 2.5)
 τ_cool = @. ((radius_chamber*1e3) * aspect_ratio)^2  * inv(thermal_diffusivity)
 
 θ1_models_normal = @. (3/4π) * (M_injection_normal_T2019 * inv(thermal_diffusivity * density_magma * (total_radius*1e3)))
-θ2_models_normal_strong = @. (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_strong) * inv(ΔPc * density_magma * (total_radius*1e3)^3)
-θ2_models_normal_weak = @. (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_weak) * inv(ΔPc * density_magma * (total_radius*1e3)^3)
-θ2_models_normal_townsend = @. (3/4π) * (M_injection_normal_T2019 * η_host_rock_Townsend2019) * inv(ΔPc * density_magma * (total_radius*1e3)^3)
+θ2_models_normal_strong = @. (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_strong) * inv(ΔPc * density_magma * radius_chamber)
+θ2_models_normal_weak = @. (3/4π) * (M_injection_normal_T2019 * η_host_rock_JustCollapse_weak) * inv(ΔPc * density_magma * radius_chamber)
+θ2_models_normal_townsend = @. (3/4π) * (M_injection_normal_T2019 * η_host_rock_Townsend2019) * inv(ΔPc * density_magma * radius_chamber)
 
 θ1_models_increased = @. (3/4π) * (M_injection_increased_T2019 * inv(thermal_diffusivity * density_magma * (total_radius*1e3)))
-θ2_models_increased_strong = @. (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_strong) * inv(ΔPc * density_magma * (total_radius*1e3)^3)
-θ2_models_increased_weak = @. (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_weak) * inv(ΔPc * density_magma * (total_radius*1e3)^3)
-θ2_models_increased_townsend = @. (3/4π) * (M_injection_increased_T2019 * η_host_rock_Townsend2019) * inv(ΔPc * density_magma * (total_radius*1e3)^3)
+θ2_models_increased_strong = @. (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_strong) * inv(ΔPc * density_magma * radius_chamber)
+θ2_models_increased_weak = @. (3/4π) * (M_injection_increased_T2019 * η_host_rock_JustCollapse_weak) * inv(ΔPc * density_magma * radius_chamber)
+θ2_models_increased_townsend = @. (3/4π) * (M_injection_increased_T2019 * η_host_rock_Townsend2019) * inv(ΔPc * density_magma * radius_chamber)
 
 
 #scaling law degruyter & huber 2014
@@ -106,84 +105,11 @@ ax = Axis(fig[1, 1],
     yticklabelsize = 20,
     xlabelsize = 24,
     ylabelsize = 24,
+    # xticks = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3],
+    # yticks = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4],
     # title = "Relaxation and Eruption Criteria"
 )
 
-
-
-#reference model with eta 1e19
-scatter!(ax,
-    θ1_ref_townsend,
-    θ2_ref_townsend;
-    marker = :star5,
-    color = :grey,
-    strokecolor = :grey,
-    strokewidth = 2,
-    label = "Reference model",
-    markersize = 24
-)
-
-#reference model with eta 1e19 and increased injection rate
-scatter!(ax,
-    θ1_ref_increased_townsend,
-    θ2_ref_increased_townsend;
-    marker = :star5,
-    color = :grey,
-    strokecolor = :grey,
-    strokewidth = 2,
-    label = "Reference model",
-    markersize = 24
-)
-
-# reference model with eta $(η_host_rock_JustCollapse)
-scatter!(ax,
-    θ1_ref_normal,
-    θ2_ref_normal_strong;
-    marker = :star5,
-    # color = colors[1],
-    color = :grey,
-    label = "Reference model",
-    strokecolor = :grey,
-    strokewidth = 2,
-    markersize = 24
-)
-
-scatter!(ax,
-    θ1_ref_normal,
-    θ2_ref_normal_weak;
-    marker = :star5,
-    # color = colors[4],
-    color = :grey,
-    label = "Reference model",
-    strokecolor = :grey,
-    strokewidth = 2,
-    markersize = 24
-)
-
-# reference model with eta $(η_host_rock_JustCollapse) and increased injection rate
-scatter!(ax,
-    θ1_ref_increased,
-    θ2_ref_increased_strong;
-    marker = :star5,
-    # color = colors[3],
-    color = :grey,
-    strokecolor = :grey,
-    strokewidth = 2,
-    label = "Reference model",
-    markersize = 24
-)
-
-scatter!(ax,
-    θ1_ref_increased,
-    θ2_ref_increased_weak;
-    marker = :star5,
-    # color = colors[5],
-    color = :grey,
-    strokecolor = :grey,
-    strokewidth = 2,
-    label = "Reference model",
-    markersize = 24
-)
 
 # Now the models w/ normal injection ra
 scatter!(ax,
@@ -280,9 +206,85 @@ scatter!(ax,
     markersize = 15
 )
 
+
+
+#reference model with eta 1e19
+scatter!(ax,
+    θ1_ref_townsend,
+    θ2_ref_townsend;
+    marker = :star5,
+    color = :grey,
+    strokecolor = :grey,
+    strokewidth = 2,
+    label = "Reference model",
+    markersize = 24
+)
+
+#reference model with eta 1e19 and increased injection rate
+scatter!(ax,
+    θ1_ref_increased_townsend,
+    θ2_ref_increased_townsend;
+    marker = :star5,
+    color = :grey,
+    strokecolor = :grey,
+    strokewidth = 2,
+    label = "Reference model",
+    markersize = 24
+)
+
+# reference model with eta $(η_host_rock_JustCollapse)
+scatter!(ax,
+    θ1_ref_normal,
+    θ2_ref_normal_strong;
+    marker = :star5,
+    # color = colors[1],
+    color = :grey,
+    label = "Reference model",
+    strokecolor = :grey,
+    strokewidth = 2,
+    markersize = 24
+)
+
+scatter!(ax,
+    θ1_ref_normal,
+    θ2_ref_normal_weak;
+    marker = :star5,
+    # color = colors[4],
+    color = :grey,
+    label = "Reference model",
+    strokecolor = :grey,
+    strokewidth = 2,
+    markersize = 24
+)
+
+# reference model with eta $(η_host_rock_JustCollapse) and increased injection rate
+scatter!(ax,
+    θ1_ref_increased,
+    θ2_ref_increased_strong;
+    marker = :star5,
+    # color = colors[3],
+    color = :grey,
+    strokecolor = :grey,
+    strokewidth = 2,
+    label = "Reference model",
+    markersize = 24
+)
+
+scatter!(ax,
+    θ1_ref_increased,
+    θ2_ref_increased_weak;
+    marker = :star5,
+    # color = colors[5],
+    color = :grey,
+    strokecolor = :grey,
+    strokewidth = 2,
+    label = "Reference model",
+    markersize = 24
+)
+
 # xlims!(ax, 1e-3, 1e3)
 xlims!(ax, 1e0, 1e3)
-ylims!(ax, 1e-3, 1e4)
+ylims!(ax, 1e-3, 1e5)
 # hlines!(ax, [1.0], color=:black, linestyle=:dash)
 # vlines!(ax, [1.0], color=:black, linestyle=:dashdot)
 
